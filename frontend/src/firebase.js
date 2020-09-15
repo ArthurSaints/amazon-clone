22d91311424258f21855986f2337bcab4c4837a2
Arthur Santos <arthursantosgc@gmail.com>
2020-09-15 11:17:29 -0300

    projeto frontend configurado e pronto para uso

---
+++ frontend/src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCZWOXxskrSMDuyYNzRDRBThGMucxP8VDM",
+    authDomain: "clone-2f055.firebaseapp.com",
+    databaseURL: "https://clone-2f055.firebaseio.com",
+    projectId: "clone-2f055",
+    storageBucket: "clone-2f055.appspot.com",
+    messagingSenderId: "797809503434",
+    appId: "1:797809503434:web:2fbf3235e554ce27ce1833",
+};
+
+export default firebaseConfig;

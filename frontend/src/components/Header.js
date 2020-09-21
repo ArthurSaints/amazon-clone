1055ddef0dc8cee9482db4070e2bd1f9232145b1
Arthur Santos <arthursantosgc@gmail.com>
2020-09-21 09:29:46 -0300

    header finalizado

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 import "./Header.css";
 
@@ -10,10 +11,11 @@ function Header() {
             <img
                 className="header__logo"
                 src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt="amazon logo"
             />
 
             <div className="header__search">
-                <input type="text" className="header_searchInput" type="text" />
+                <input type="text" className="header__searchInput" />
                 <SearchIcon className="header__searchIcon" />
             </div>
 
@@ -24,12 +26,17 @@ function Header() {
                 </div>
                 <div className="header__option">
                     <span className="header__optionLineOne">Returns</span>
-                    <span className="header__optionLineTwo">&Orders</span>
+                    <span className="header__optionLineTwo">&    Orders</span>
                 </div>
                 <div className="header__option">
                     <span className="header__optionLineOne">Your</span>
                     <span className="header__optionLineTwo">Prime</span>
                 </div>
+
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header__optionLineTwo header__basketCount">0</span>
+                </div>        
             </div>
         </div>
     );

aaf30ad37cd2bdb0fd037241b70ce9ddebc57c2c
Arthur Santos <arthursantosgc@gmail.com>
2020-09-24 11:49:48 -0300

    manipulaçao de estados atraves do stateprovider e reducer

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,8 +1,27 @@
 import React from "react";
 
+import { useStateValue } from "../StateProvider";
+
 import "./Product.css";
 
 function Product({ id, title, image, price, rating }) {
+    const [{ basket }, dispatch] = useStateValue();
+
+    console.log("this is the basket > ", basket);
+
+    const addToBasket = () => {
+        dispatch({
+            type: "ADD_TO_BASKET",
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            },
+        });
+    };
+
     return (
         <div className="product">
             <div className="product__info">
@@ -16,12 +35,12 @@ function Product({ id, title, image, price, rating }) {
                         .fill()
                         .map((_, i) => (
                             //eslint-disable-next-line
-                            <p>🌟</p>
+                            <p key={i}>🌟</p>
                         ))}
                 </div>
             </div>
             <img src={image} alt="product" />
-            <button>Add to basket</button>
+            <button onClick={addToBasket}>Add to basket</button>
         </div>
     );
 }

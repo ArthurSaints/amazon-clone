ddca37a5a6ed33ac674527beabd1f4f4d7b60c63
Arthur Santos <arthursantosgc@gmail.com>
2020-09-24 09:45:39 -0300

    home page finalizada

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import amazonBanner from "../images/amazon-prime-banner.jpeg";
-
 import Product from "./Product";
 
 import "./Home.css";
@@ -10,28 +8,64 @@ function Home() {
     return (
         <div className="home">
             <div className="home__container">
-                <img 
+                <img
                     className="home__image"
-                    src={amazonBanner} 
-                    alt="amazon_banner" />
-            </div>
+                    src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+                    alt="amazon_banner"
+                />
+                <div className="home__row">
+                    <Product
+                        id="123"
+                        title="The Lean Startup"
+                        price={29.99}
+                        image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+                        rating={5}
+                    />
+                    <Product
+                        id="123"
+                        title="The Lean Startup"
+                        price={29.99}
+                        image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+                        rating={5}
+                    />
+                </div>
 
-            <div className="home__row">
-                <Product />
-                <Product />
-            </div>
+                <div className="home__row">
+                    <Product
+                        id="123"
+                        title="The Lean Startup"
+                        price={29.99}
+                        image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+                        rating={5}
+                    />
+                    <Product
+                        id="123"
+                        title="The Lean Startup"
+                        price={29.99}
+                        image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+                        rating={5}
+                    />
+                    <Product
+                        id="123"
+                        title="The Lean Startup"
+                        price={29.99}
+                        image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+                        rating={5}
+                    />
+                </div>
 
-            <div className="home__row">
-                <Product />
-                <Product />
-                <Product />
-            </div>
-
-            <div className="home__row">
-                <Product />
+                <div className="home__row">
+                    <Product
+                        id="123"
+                        title="The Lean Startup"
+                        price={29.99}
+                        image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+                        rating={5}
+                    />
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
-function Product() {
+import "./Product.css";
+
+function Product({ id, title, image, price, rating }) {
     return (
         <div className="product">
-            product
+            <div className="product__info">
+                <p>{title}</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                            //eslint-disable-next-line
+                            <p>🌟</p>
+                        ))}
+                </div>
+            </div>
+            <img src={image} alt="product" />
+            <button>Add to basket</button>
         </div>
-    )
+    );
 }
 
-export default Product
+export default Product;
